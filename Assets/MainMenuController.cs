c9b851ed4b195e3d1ff2d297ed895aa1c3eb0a5f
Virimine <47324439+Virimine@users.noreply.github.com>
2021-01-31 15:57:15 +0200

    UI animations

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -6,16 +6,22 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour {
 
 	[SerializeField] Button playButton = default;
-    [SerializeField] Button creditsButton = default;
+	[SerializeField] Button creditsButton = default;
 	[SerializeField] Button backButton = default;
-
+	[SerializeField] Animator buttonsAnim = default;
+	[SerializeField] Animator cameraAnim = default;
+	
 	void Start() {
-        playButton.onClick.AddListener(Play);
-        creditsButton.onClick.AddListener(Credits);
-        backButton.onClick.AddListener(UnCredits);
-    }
+		playButton.onClick.AddListener(Play);
+		creditsButton.onClick.AddListener(Credits);
+		backButton.onClick.AddListener(UnCredits);
+	}
 
-    void Play() { }
-    void Credits() { }
-    void UnCredits() { }
+	void Play() {
+		cameraAnim.Play("Play");
+		playButton.onClick.RemoveAllListeners();
+		creditsButton.onClick.RemoveAllListeners();
+	}
+	void Credits() { buttonsAnim.Play("Credits"); }
+	void UnCredits() { buttonsAnim.Play("Back"); }
 }
